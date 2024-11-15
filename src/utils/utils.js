da82abc6033dce85f898d59ec529719a647f905b
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-15 18:36:34 +0700

    Update utils.js

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,1 +1,10 @@
-
+export async function displayHeader() { 
+  const chalk = await import('chalk');
+  console.log("");
+  console.log(chalk.default.yellow(" ============================================"));
+  console.log(chalk.default.yellow("|        BLOCKLESS BELSS NETWORK BOT        |"));
+  console.log(chalk.default.yellow("|           AUTHOR : NOFAN RAMBE            |"));
+  console.log(chalk.default.yellow("|           WELCOME & ENJOY SIR!            |")); 
+  console.log(chalk.default.yellow(" ============================================"));
+  console.log("");
+}
