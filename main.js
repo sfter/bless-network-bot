8f9031d6fa8c8588a98ff786fd5396b9d875b1ea
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-15 19:01:07 +0700

    Update main.js

--- main.js
+++ main.js
@@ -144,11 +144,11 @@ async function fetchIpAddress(fetch, agent) {
 async function displayHeader() {
 const chalk = await import('chalk');
   console.log("");
-  console.log(chalk.default.yellow(" ============================================"));
-  console.log(chalk.default.yellow("|        BLOCKLESS BELSS NETWORK BOT        |"));
-  console.log(chalk.default.yellow("|           AUTHOR : NOFAN RAMBE            |"));
-  console.log(chalk.default.yellow("|           WELCOME & ENJOY SIR!            |")); 
-  console.log(chalk.default.yellow(" ============================================"));
+  console.log(chalk.default.yellow("============================================"));
+  console.log(chalk.default.yellow("|        BLOCKLESS BELSS NETWORK BOT       |"));
+  console.log(chalk.default.yellow("|           AUTHOR : NOFAN RAMBE           |"));
+  console.log(chalk.default.yellow("|           WELCOME & ENJOY SIR!           |")); 
+  console.log(chalk.default.yellow("============================================"));
   console.log("");
 }
 async function runAll() {
